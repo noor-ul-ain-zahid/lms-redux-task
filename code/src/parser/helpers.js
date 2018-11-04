47315b899d39a17cd400c2fc3e3b016443da1d82
noor-ul-ain-zahid <noorulaincs2@gmail.com>
2018-11-04 18:21:02 +0500

    lms-redux-task

--- code/src/parser/helpers.js
+++ code/src/parser/helpers.js
@@ -1,21 +1,21 @@
-export const edit = (oldData, oldPublisher, data) => {
-    let newData = oldData
-    const index = newData.indexOf(oldPublisher)
-    newData.splice(index, 1, data)
-    return newData
+export const edit = (dataSet, oldData, newData) => {
+    let newDataSet = dataSet
+    const index = newDataSet.indexOf(oldData)
+    newDataSet.splice(index, 1, newData)
+    return newDataSet
 }
 export const del = (oldData, data) => {
     let newData = oldData
     const index = newData.indexOf(data)
-     newData.splice(index, 1)
-     return newData
+    newData.splice(index, 1)
+    return newData
 }
-export const notExisting = (data,newData) => {
-    const check =(data.filter((item) => (item.name === newData.name && item.id != newData.id)).length == 0)
+export const notExisting = (data, newData) => {
+    const check = (data.filter((item) => (item.name === newData.name && item.id != newData.id)).length == 0)
     return check;
 }
-export const Delete=(data,type,name)=>{
-    const newData =(data.filter((item) => item[type]!=name))
+export const deleteBooks = (data, type, name) => {
+    const newData = (data.filter((item) => item[type] != name))
     return newData;
 }
 
